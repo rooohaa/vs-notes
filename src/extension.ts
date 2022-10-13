6c32c0ce6a2be5be652be09ade7a15df38a7b6bc
Rakhat Alpysbai <rakhatdev@gmail.com>
2022-10-13 15:26:33 +0600

    remove unused code

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import { authenticate } from './authenticate';
 import { SidebarProvider } from './SidebarProvider';
 import { TokenManager } from './TokenManager';
 
@@ -12,16 +11,6 @@ export function activate(context: vscode.ExtensionContext) {
       'vs-notes-sidebar',
       sidebarProvider
     )
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand('vs-notes.authenticate', () => {
-      try {
-        authenticate();
-      } catch (err) {
-        console.log(err);
-      }
-    })
   );
 
   context.subscriptions.push(
